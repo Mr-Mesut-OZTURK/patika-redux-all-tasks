e0d2b9b1076ffef524fc5be1d40278aad778ff62
Mesut OZTURK <mesut8311006@gmail.com>
2023-08-30 14:18:52 +0300

    weather app finished

--- src/apps/weather/Weather.tsx
+++ src/apps/weather/Weather.tsx
@@ -1,7 +1,155 @@
+import { Box, CircularProgress, FormControl, InputLabel, MenuItem, Select, Stack, Typography } from "@mui/material"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { BackButton } from "src/components"
+import { useAppDispatch, useAppSelector } from "src/redux/hooks"
+import { setSelectedAreaWeatherState, setWeatherSelectedCity } from "src/redux/slices/weatherSlice"
+
+
 
 const Weather = () => {
+
+    // console.log(import.meta.env.VITE_OPEN_WEATHER_API_KEY)
+
+    const dispatch = useAppDispatch()
+    const { weatherSelectedCity, selectedAreaWeatherState } = useAppSelector(state => state.weather)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(true)
+        if (weatherSelectedCity) {
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${weatherSelectedCity}&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`)
+                .then(response => {
+                    // console.log({ response: response?.data })
+                    dispatch(setSelectedAreaWeatherState(response?.data))
+                    setLoading(false)
+                })
+                .catch(err => {
+                    console.log({ err })
+                })
+        } else {
+            const successCallback = (position: unknown) => {
+                // console.log(position);
+
+                axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position?.coords.latitude}&lon=${position?.coords.longitude}&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`)
+                    .then(response => {
+                        // console.log({ response: response?.data })
+                        dispatch(setSelectedAreaWeatherState(response?.data))
+                        setLoading(false)
+                    })
+                    .catch(err => {
+                        console.log({ err })
+                    })
+
+            };
+
+            const errorCallback = (error: unknown) => {
+                console.log(error);
+            };
+
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+        }
+    }, [dispatch, weatherSelectedCity])
+
+
+
+    const handleSetWeatherSelectedCity = (value: string) => {
+        dispatch(setWeatherSelectedCity(value))
+    }
+
+
+
+    console.log({ selectedAreaWeatherState })
+
     return (
-        <div>Weather</div>
+        <Box
+            sx={{
+                position: 'relative',
+                // backgroundColor: '#ffc93c',
+                p: 3,
+                pt: '90px',
+                borderRadius: 1,
+            }}
+            boxShadow={10}
+        >
+
+            <BackButton style={{ top: 25, left: 25 }} />
+
+
+            <FormControl variant="standard" sx={{ maxWidth: 600, display: 'flex', m: 'auto', my: 5 }} fullWidth>
+                <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
+                <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={weatherSelectedCity}
+                    onChange={(e) => handleSetWeatherSelectedCity(e.target.value)}
+                    label="Age"
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value="istanbul">İstanbul</MenuItem>
+                    <MenuItem value="izmir">izmir</MenuItem>
+                    <MenuItem value="ankara">Ankara</MenuItem>
+                </Select>
+            </FormControl>
+
+
+            <Box
+                sx={{
+                    p: 3,
+                    pt: 5,
+                    boxShadow: 6,
+                }}
+            >
+
+                {
+                    loading ? (
+                        <Stack p={5} alignItems="center" justifyContent="center">
+                            <CircularProgress color="secondary" />
+                        </Stack>
+                    ) : (
+                        <Stack>
+
+                            <Stack>
+                                <Typography>
+                                    Hava durumu : {selectedAreaWeatherState?.weather?.[0]?.main}
+                                </Typography>
+                                <Typography>
+                                    Açıklama : {selectedAreaWeatherState?.weather?.[0]?.description}
+                                </Typography>
+                                {/* <Typography>
+                                    İkon : {parse(`&#${selectedAreaWeatherState?.weather?.[0]?.icon};`)} &#01d;
+                                </Typography> */}
+
+                            </Stack>
+
+
+
+                            <Typography>
+                                Hissedilen : {selectedAreaWeatherState?.main?.feels_like}
+                            </Typography>
+                            <Typography>
+                                Nem : {selectedAreaWeatherState?.main?.humidity}
+                            </Typography>
+                            <Typography>
+                                Basınç : {selectedAreaWeatherState?.main?.pressure}
+                            </Typography>
+                            <Typography>
+                                Sıcaklık : {selectedAreaWeatherState?.main?.temp}
+                            </Typography>
+                            <Typography>
+                                En yüksek sıcaklık : {selectedAreaWeatherState?.main?.temp_max}
+                            </Typography>
+                            <Typography>
+                                En düşük sıcaklık : {selectedAreaWeatherState?.main?.temp_min}
+                            </Typography>
+                        </Stack>
+                    )
+                }
+            </Box>
+
+        </Box>
     )
 }
 
--- src/redux/slices/weatherSlice.ts
+++ src/redux/slices/weatherSlice.ts
@@ -2,24 +2,42 @@ import { createSlice } from '@reduxjs/toolkit'
 
 
 export interface WeatherState {
-    value: number
+    weatherSelectedCity: string,
+    selectedAreaWeatherState: {
+        main:{
+            temp : number,
+            feels_like : number,
+            temp_min : number,
+            temp_max : number,
+            pressure : number,
+            humidity : number
+        } | null,
+        weather:[{
+            main:string,
+            description:string
+        }] | null
+    } | null,
 }
 
 const initialState: WeatherState = {
-    value: 0
+    weatherSelectedCity: "",
+    selectedAreaWeatherState: null,
 }
 
 export const weatherSlice = createSlice({
     name: 'weather',
     initialState,
     reducers: {
-        setValue: (state, action) => {
-            console.log({state, action})
+        setWeatherSelectedCity: (state, action) => {
+            state.weatherSelectedCity = action.payload
+        },
+        setSelectedAreaWeatherState: (state, action) => {
+            state.selectedAreaWeatherState = action.payload
         }
     }
 })
 
 
-export const { setValue} = weatherSlice.actions
+export const { setWeatherSelectedCity,setSelectedAreaWeatherState } = weatherSlice.actions
 
 export default weatherSlice.reducer
